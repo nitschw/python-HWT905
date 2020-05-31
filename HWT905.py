10c301b0b810b03355f0549552cac2b6c8f7a7e2
wnitsch <wnitsch@greensightag.com>
2020-05-31 12:39:53 -0400

    Got the data working via getReading(), planning to extend further to get all the special data elements working, like time etc.

--- HWT905.py
+++ HWT905.py
@@ -1,7 +1,4 @@
-import serial
-from serial import Serial
-
-# /dev/tty.usbserial-14520
+import serial as s
 
 class HWT905():
     def __init__(self, port, baud):
@@ -16,57 +13,98 @@ class HWT905():
         self.x_magnetic_bias = 0
         self.y_magnetic_bias = 0
         self.z_magnetic_bias = 0
-
-        self.ser = Serial(port, baudrate=baud)
+        self.ax = 0
+        self.ay = 0
+        self.az = 0
+        self.wx = 0
+        self.wy = 0
+        self.wz = 0
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+        self.hx = 0
+        self.hy = 0
+        self.hz = 0
+        self.time_year = 0
+        self.time_month = 0
+        self.time_day = 0
+        self.time_hour = 0
+        self.time_second = 0
+        self.time_milli = 0
+        self.acc_temperature = 0
+        self.ser = s.Serial(port, baudrate=baud)
 
     def closeSerial(self):
         self.ser.close()
 
-    def hexify(self, data):
-        data_int = []
-        print(data)
-        for i in range(0, len(data), 2):
-            data_int.append(int('0x'+data[i:i+2],16))
-        print(data_int)
-        return data_int
-
     def getReading(self):
+        self.ser.flush()
         data = b'0'
-        for i in range(4):
+        for i in range(6):
             while data.hex() != '55':
                 data = self.ser.read(1) 
             data = self.ser.read(1)
-            if data.hex() == '51':  # Acceleration output
+            if data.hex() == '50':  # Time output
+                print('Got time data')
+                data = self.ser.read(9)
+                data_int = self.unhexify(data.hex())
+                self.time_year = 2000 + data_int[0]
+                self.time_month = data_int[1]
+                self.time_day = data_int[2]
+                self.time_hour = data_int[3]
+                self.time_minute = data_int[4]
+                self.time_milli = ((data_int[6] << 8) | data_int[5])
+            elif data.hex() == '51':  # Acceleration output
                 print('Got acceleration data')
                 data = self.ser.read(9)
-                data_int = self.hexify(data.hex())
-                self.ax = (data_int[1] << 8 + data_int[0])/32768*16*9.81
-                self.ay = (data_int[3] << 8 + data_int[2])/32768*16*9.81
-                self.az = (data_int[5] << 8 + data_int[4])/32768*16*9.81
-                self.acc_temperature = (data_int[7] << 8 + data_int[6])/100
+                data_int = self.unhexify(data.hex())
+                self.ax = ((data_int[1] << 8) | data_int[0])/32768*16*9.81
+                self.ay = ((data_int[3] << 8) | data_int[2])/32768*16*9.81
+                self.az = ((data_int[5] << 8) | data_int[4])/32768*16*9.81
+                self.acc_temperature = ((data_int[7] << 8) + data_int[6])/100
             elif data.hex() == '52':  # Angular velocity output
                 print('Got angular velocity data')
                 data = self.ser.read(9)
-                data_int = self.hexify(data.hex())
-                self.wx = (data_int[1] << 8 + data_int[0])/32768*2000
-                self.wy = (data_int[3] << 8 + data_int[2])/32768*2000
-                self.wz = (data_int[5] << 8 + data_int[4])/32768*2000
-                self.w_temperature = (data_int[7] << 8 + data_int[6])/100
+                data_int = self.unhexify(data.hex())
+                self.wx = ((data_int[1] << 8) | data_int[0])/32768*2000
+                self.wy = ((data_int[3] << 8) | data_int[2])/32768*2000
+                self.wz = ((data_int[5] << 8) | data_int[4])/32768*2000
+                self.w_temperature = ((data_int[7] << 8) | data_int[6])/100
             elif data.hex() == '53':  # Angle output
                 print('Got angular data output')
                 data = self.ser.read(9)
-                data_int = self.hexify(data.hex())
-                self.roll = (data_int[1] << 8 + data_int[0])/32768*180
-                self.pitch = (data_int[3] << 8 + data_int[2])/32768*180
-                self.yaw = (data_int[5] << 8 + data_int[6])/32768*180
-                self.rpy_temperature = (data_int[7] << 8 + data_int[8])/100
+                data_int = self.unhexify(data.hex())
+                self.roll = ((data_int[1] << 8) | data_int[0])/32768*180
+                self.pitch = ((data_int[3] << 8) | data_int[2])/32768*180
+                self.yaw = ((data_int[5] << 8) | data_int[6])/32768*180
+                self.rpy_temperature = ((data_int[7] << 8) | data_int[8])/100
             elif data.hex() == '54':  # Magnetometer output
                 print('Got magnetometer data output')
                 data = self.ser.read(9)
-                data_int = self.hexify(data.hex())
-                self.hx = data_int[1] << 8 + data_int[0]
-                self.hy = data_int[3] << 8 + data_int[2]
-                self.hz = data_int[5] << 8 + data_int[4]
+                data_int = self.unhexify(data.hex())
+                self.hx = (data_int[1] << 8) | data_int[0]
+                self.hy = (data_int[3] << 8) | data_int[2]
+                self.hz = (data_int[5] << 8) | data_int[4]
+        print('time_year: ', self.time_year)
+        print('ax: ', self.ax)
+        print('ay: ', self.ay)
+        print('az: ', self.az)
+        print('accel_temp: ', self.acc_temperature)
+        print('wx: ', self.wx)
+        print('wy: ', self.wy)
+        print('wz: ', self.wz)
+        print('roll: ', self.roll)
+        print('pitch: ', self.pitch)
+        print('yaw: ', self.yaw)
+        print('hx: ', self.hx)
+        print('hy: ', self.hy)
+        print('hz: ', self.hz)
+
+    def unhexify(self, data):
+        data_int = []
+        for i in range(0, len(data), 2):
+            data_int.append(int('0x'+data[i:i+2],16))
+        return data_int
 
     def setBaud(self, baud):
         if baud == 2400:
@@ -149,6 +187,9 @@ class HWT905():
         self.writeBytes(bytes)
 
     def selectAlgorithm(self, alg):
+        """
+        make a proper docstring
+        """
         if alg == 6:
             alg_byte = 0x00
         elif alg == 9:
@@ -158,7 +199,92 @@ class HWT905():
                   ' 9-axis (input: 9) mode')
             return
         self.algorithm = alg
-        bytes = [0xFF, 0xAA, 0x24, alg_byte, 0x00]        
+        bytes = [0xFF, 0xAA, 0x24, alg_byte, 0x00]
+
+    def change_char(self, s, p, r):
+        return s[:p]+r+s[p+1:]
+
+    def setReturnContent(self, content_dict=None):
+        """
+        The setReturnContent function takes a dict "content_dict" and uses it to
+        set the values the HWT905 device will be spamming in it's serial output.
+        Set the fields in the dict to true or false and they will or will not be
+        included in your data.
+        
+        content_dict: this variable should be a list with a boolean 
+                      entry for each possible content type that can be
+                      contained in the output message
+                      
+                      From the manual, here are the content types:
+                      0x50 pack: time    (doesn't seem to work)
+                      0x51 pack: acceleration
+                      0x52 pack: angular velocity
+                      0x53 pack: angle
+                      0x54 pack: magnetometer
+                      0x55 pack: port status    (doesn't seem to work)
+                      0x56 pack: atmospheric pressure and height (doesn't seem to work)
+                      0x57 pack: lat/lon output (I doubt this is supported)
+                      0x58 pack: GPS speed (or this? how does it use GPS wtf?)
+                      0x59 pack: quaternion
+                      0x5A pack: satellite position accuracy (is this real? what satellites is this thing using)
+                      
+                      "content_dict" should be a dict with the fields above, as shown below:
+                      
+                      content_dict = {'time', True,
+                                      'acceleration', True,
+                                      'angular_vel, True,
+                                      'angle', True,
+                                      'magnetometer', True,
+                                      'port_status', True,
+                                      'atmo', True,
+                                      'lat_lon', True,
+                                      'gps_speed', True,
+                                      'quaternion', True,
+                                      'sat_pos_acc', True}
+        """
+        if content_dict is None:
+            content_dict = {'time': True,
+                            'acceleration': True,
+                            'angular_vel': True,
+                            'angle': True,
+                            'magnetometer': True,
+                            'port_status': True,
+                            'atmo': True,
+                            'lat_lon': True,
+                            'gps_speed': True,
+                            'quaternion': True,
+                            'sat_pos_acc': True}
+
+        class content:
+            def __init__(self, var, idx, val):
+                self.variable = var
+                self.idx = idx
+                self.val = val
+
+        # this makes sense if you refer to the manual's "set return content" section 1.2.8
+        time = content('RSWL', 7, content_dict['time'])
+        acceleration = content('RSWL', 6, content_dict['acceleration'])
+        angular_vel = content('RSWL', 5, content_dict['angular_vel'])
+        angle = content('RSWL', 4, content_dict['angle'])
+        magnetometer = content('RSWL', 3, content_dict['magnetometer'])
+        port_status = content('RSWL', 2, content_dict['port_status'])
+        atmo = content('RSWL', 1, content_dict['atmo'])
+        lat_lon = content('RSWL', 0, content_dict['lat_lon'])
+        gps_speed = content('RSWH', 7, content_dict['gps_speed'])
+        quaternion = content('RSWH', 6, content_dict['quaternion'])
+        sat_pos_acc = content('RSWH', 5, content_dict['sat_pos_acc'])
+                
+        RSWL = list('00000000')
+        RSWH = list('00000000')
+        for stat in content_dict.keys():
+            val = str(int(vars()[stat].val))
+            idx = int(vars()[stat].idx)
+            vars()[vars()[stat].variable][idx] = val
+        RSWL = int("".join(RSWL), 2)
+        RSWH = int("".join(RSWH), 2)
+
+        bytes = [0xFF, 0xAA, 0x02, RSWL, RSWH]
+        self.writeBytes(bytes)
 
     def resetDefaultConfig(self):
         self.writeBytes([0xFF, 0xAA, 0x00, 0x01, 0x00])
@@ -166,9 +292,7 @@ class HWT905():
     def toggleSleep(self):
         self.writeBytes([0xFF, 0xAA, 0x22, 0x01, 0x00])
 
-    
-
     #def calibrate():
 
     def writeBytes(self, bytes):
-        self.ser.write(serial.to_bytes(bytes))
+        self.ser.write(s.to_bytes(bytes))
